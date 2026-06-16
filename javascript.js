fd86be42ae81417e2f3284a229ee09e4c09e4e08
udayan-banerjee <udayanbanerjee04@gmail.com>
2026-06-16 17:40:58 +0530

    created basic design

---
+++ javascript.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+let n1, n2, oper;
+
+function operate (a, op, b) {
+    switch (op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
